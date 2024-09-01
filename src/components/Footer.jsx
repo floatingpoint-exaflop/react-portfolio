a8b02946d763d47418a4b84e353fbb938f133a4b
Frederico Swarthings <timscallon1@gmail.com>
2024-09-01 18:33:24 -0500

    further scaffolding and cleanup, started work

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import '../App.css'
+
+function Footer() {
+
+    return (
+      <>
+        <h1>Tim Scallon</h1>
+        <p>I am an aspiring developer based in North Minneapolis</p>
+      </>
+    )
+  }
+  
+  export default Footer
---
+++ src/components/Project.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import '../App.css'
+
+function Project() {
+
+    return (
+      <>
+        <h1>Tim Scallon</h1>
+        <p>I am an aspiring developer based in North Minneapolis</p>
+      </>
+    )
+  }
+  
+  export default Project
