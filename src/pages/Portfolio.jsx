ed7e7341e253f157d0efc5f7ee7fc8411e958c4f
Frederico Swarthings <timscallon1@gmail.com>
2024-09-14 16:13:42 -0500

    updated project list and resume dates

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -4,24 +4,24 @@ export default function Portfolio() {
   const projectList = [
     {
       id: 1,
+      img: "/assets/eatitorbeatit.png",
+      label: "Recipe Search n' Save",
+      repoHref: "https://github.com/TeutonicTed/project-03",
+      deployHref: "https://project-03-yel1.onrender.com/",
+    },
+    {
+      id: 2,
       img: "/assets/exposition-inn.png",
       label: "DND Character Builder",
       repoHref: "https://github.com/JonGrom/Exposition_Inn",
       deployHref: "https://exposition-inn.onrender.com",
     },
     {
-      id: 2,
+      id: 3,
       img: "/assets/war-cards.png",
       label: "War: The Card Game",
       repoHref: "https://github.com/floatingpoint-exaflop/group-project-one",
       deployHref: "https://floatingpoint-exaflop.github.io/group-project-one/",
-    },
-    {
-      id: 3,
-      img: "/assets/mvc-tech-blog.png",
-      label: "MVC Tech Blog",
-      repoHref: "https://github.com/floatingpoint-exaflop/mvc-tech-blog",
-      deployHref: "https://mvc-tech-blog-604q.onrender.com",
     },
     {
       id: 4,
